b3eec6a55c4eee0aea1e808bf553c1d5c9ff87b3
Yegor Dargin <darginyegor@gmail.com>
2020-10-22 12:12:22 +0300

    one incredibly small optimization

--- main.py
+++ main.py
@@ -15,22 +15,24 @@ def main():
     tn.read_until(b"#")
     tn.write(b"ls\n")
     files_string = tn.read_until(b"#").decode("ascii")
-    files = [m for m in re.findall('VID[0-9]*.MP4', files_string)]
+    files = [v for v in re.findall('VID[0-9]*.MP4', files_string)]
     print("Found " + str(len(files)) + " videos. Download the last (L) one or all (A)?")
     download_option = input()
+    # Creating directory to store videos
+    path = str(Path.home()) + "/ghost-videos/"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    # Downloading videos
     if download_option.lower() == "l":
-        download(files[-1])
+        download(files[-1], path)
     elif download_option.lower() == 'a':
         for file in files:
-            download(file)
+            download(file, path)
 
 
-def download(filename):
+def download(filename, path):
     print("Downloading " + filename + "...")
     url = "http://192.168.42.1/DCIM/100MEDIA/" + filename
     r = requests.get(url)
-    path = str(Path.home()) + "/ghost-videos/"
-    Path(path).mkdir(parents=True, exist_ok=True)
     f = open(path + filename, "wb+")
     f.write(r.content)
     f.close()
